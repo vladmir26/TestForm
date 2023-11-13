2d8b81a7467232b843b0b29b8041b4330d285e98
vladmir26 <97904721+vladmir26@users.noreply.github.com>
2023-11-13 20:20:13 +0300

    Marked up the form

---
+++ App.vue
@@ -0,0 +1,66 @@
+<script>
+</script>
+
+<template>
+ <form>
+    <label>Фамилия</label>
+    <input>
+    <label>Имя</label>
+    <input>
+    <label>Отчество</label>
+    <input>
+    <label>Дата рождения</label>
+    <input>
+    <label>Номер телефона</label>
+    <input>
+    <label>Пол</label>
+    <input>
+    <label>Группа клиентов</label>
+    <select multiple>
+        <option>VIP</option>
+        <option>Проблемные</option>
+        <option>ОМС</option>
+    </select>
+    <label>Лечащий врач</label>
+    <select>
+        <option>Иванов</option>
+        <option>Захаров</option>
+        <option>Черышева</option>
+    </select>
+    <label>Не отправлять СМС</label>
+    <input type="checkbox">
+    <p>Адрес</p>
+    <label>Индекс</label>
+    <input>
+    <label>Страна</label>
+    <input>
+    <label>Область</label>
+    <input>
+    <label>Город</label>
+    <input>
+    <label>Улица</label>
+    <input>
+    <label>Дом</label>
+    <input>
+    <label>Индекс</label>
+    <input>
+    <p>Паспорт</p>
+    <label>Тип документа</label>
+    <select>
+        <option>Паспорт</option>
+        <option>Свидетельство о рождении</option>
+        <option>Вод.удостоверение</option>
+    </select>
+    <label>Серия</label>
+    <input>
+    <label>Номер</label>
+    <input>
+    <label>Кем выдан</label>
+    <input>
+    <label>Дата выдачи</label>
+    <input>
+ </form>
+</template>
+
+<style>
+</style>
