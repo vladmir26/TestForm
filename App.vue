abb889807e8db0c01171631c215f01e05a61fa3d
vladmir26 <97904721+vladmir26@users.noreply.github.com>
2023-11-17 15:46:54 +0300

    add dist

--- App.vue
+++ App.vue
@@ -1,17 +1,82 @@
 <script>
+import { reactive } from 'vue'
+import { useVuelidate } from '@vuelidate/core'
+import { required, helpers, alpha, numeric } from '@vuelidate/validators';
+
+export default {
+    setup() {
+        const state = reactive({
+            firstName: '',
+            lastName: '',
+            patronymic: '',
+            dateBirth: '',
+        })
+        const rules = {
+            firstName: { 
+                         required: helpers.withMessage('Это поле обязательное для заполнения', required), 
+                         alpha: helpers.withMessage('Введите имя', alpha),
+            },
+            lastName: { 
+                required:  helpers.withMessage('Это поле обязательное для заполнения', required),
+                alpha: helpers.withMessage('Введите фамилию', alpha),        
+            },
+            patronymic: {
+                alpha: helpers.withMessage('Введите отчество', alpha),      
+            },
+            dateBirth: {
+                numeric: helpers.withMessage('Введите дату рождения', numeric),
+            }
+        }
+
+        const v$ = useVuelidate(rules, state)
+        return { state, v$ }
+    },
+    methods: {
+      async submitForm() {
+        const result = await this.v$.$validate()
+        if (!result) {
+            console.log('error');
+            return
+        }
+        console.log('submit', this.state.firstName)
+       }
+    }
+}
 </script>
 
 <template>
- <form>
-    <label>Фамилия</label>
-    <input>
-    <label>Имя</label>
-    <input>
-    <label>Отчество</label>
-    <input>
-    <label>Дата рождения</label>
-    <input>
-    <label>Номер телефона</label>
+ <form @submit.prevent="submitForm">
+    <div class="wrapper">
+    <label>
+        Фамилия
+        <input type="text" v-model="state.lastName">
+        <div class="input-errors" v-for="error of v$.lastName.$errors" :key="error.$uid">
+           <div class="error-msg">{{ error.$message }}</div>
+        </div>
+    </label>
+    <label>
+        Имя
+        <input type="text" v-model="state.firstName">
+          <div class="input-errors" v-for="error of v$.firstName.$errors" :key="error.$uid">
+              <div class="error-msg">{{ error.$message }}</div>
+          </div>
+    </label>
+    <label>
+        Отчество
+        <input type="text" v-model="state.patronymic">
+          <div class="input-errors" v-for="error of v$.patronymic.$errors" :key="error.$uid">
+              <div class="error-msg">{{ error.$message }}</div>
+          </div>
+        <label>
+          Дата рождения
+          <input type="date" v-model="state.dateBirth">
+            <div class="input-errors" v-for="error of v$.dateBirth.$errors" :key="error.$uid">
+              <div class="error-msg">{{ error.$message }}</div>
+            </div>
+    </label>
+    </label>
+    </div>
+    <label type="tel">Номер телефона</label>
     <input>
     <label>Пол</label>
     <input>
@@ -33,16 +98,14 @@
     <label>Индекс</label>
     <input>
     <label>Страна</label>
-    <input>
+    <input type="text">
     <label>Область</label>
-    <input>
+    <input type="text">
     <label>Город</label>
-    <input>
+    <input type="text">
     <label>Улица</label>
-    <input>
+    <input type="text">
     <label>Дом</label>
-    <input>
-    <label>Индекс</label>
     <input>
     <p>Паспорт</p>
     <label>Тип документа</label>
@@ -56,11 +119,19 @@
     <label>Номер</label>
     <input>
     <label>Кем выдан</label>
-    <input>
+    <input type="text">
     <label>Дата выдачи</label>
-    <input>
+    <input type="date">
+    <input type="submit" value="Отправить">
  </form>
 </template>
 
 <style>
+.wrapper {
+    display: flex;
+    flex-direction: column;
+}
+.error-msg {
+    color: red;
+}
 </style>
--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue'; 
+import Vuelidate from 'vuelidate';
+
 
 new Vue({
   el: '#app',
   render: h => h(App)
 });
+
+Vue.use(Vuelidate);
