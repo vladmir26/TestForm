3e93d7ae7dc5beb77956eab7d05bd11c179cb5b5
Миронов Владислав <97904721+vladmir26@users.noreply.github.com>
2023-11-17 17:10:05 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/Test-form/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
